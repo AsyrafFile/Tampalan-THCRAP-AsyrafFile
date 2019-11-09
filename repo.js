3796fa72de44a2ccb9930b4b7f45493c8a59788f
AsyrafFile <40899099+AsyrafFile@users.noreply.github.com>
2019-11-09 14:01:09 +0800

    Update repo.js

--- repo.js
+++ repo.js
@@ -2,7 +2,7 @@
 	"contact": "AsyrafFile#8823",
 	"id": "AsyrafFile",
 	"patches": {
-	"ms-eastern": "[WIP] Replace lang_ms western_name_order to eastern_name_order_hepburn."
+	"ms-eastern": "Replace lang_ms western_name_order to eastern_name_order_hepburn."
 	},
 	"servers": [
 		"https://mirrors.thpatch.net/AsyrafFile/"
