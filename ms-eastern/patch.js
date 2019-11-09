7f6fba6f5b7de525c27f8a4a58181684862c72e7
AsyrafFile <40899099+AsyrafFile@users.noreply.github.com>
2019-11-09 09:45:34 +0800

    Added ms-eastern
    Testing stuff

---
+++ ms-eastern/patch.js
@@ -0,0 +1,14 @@
+{
+	"dependencies": [
+		"nmlgc/base_tsa",
+		"nmlgc/script_latin",
+		"nmlgc/eastern_name_order_hepburn"
+		"thpatch/lang_ms"
+	],
+	"id": "ms-eastern",
+	"servers": [
+		"https://raw.githubusercontent.com/AsyrafFile/Tampalan-THCRAP-AsyrafFile/master/ms-eastern/"
+		"https://mirrors.thpatch.net/AsyrafFile/ms-eastern/"
+	],
+	"title": "Replace lang_ms western_name_order to eastern_name_order_hepburn."
+}
---
+++ repo.js
@@ -0,0 +1,12 @@
+{
+	"contact": "AsyrafFile#8823",
+	"id": "AsyrafFile",
+	"patches": {
+		"ms-eastern": "Replace lang_ms western_name_order to eastern_name_order_hepburn.",
+	},
+	"servers": [
+		"https://raw.githubusercontent.com/AsyrafFile/Tampalan-THCRAP-AsyrafFile/master/"
+		"https://mirrors.thpatch.net/AsyrafFile/"
+	],
+	"title": "Tampalan THCRAP AsyrafFile"
+}
