b95518f71b00c131fab3c2d6f90c8fbb90c7cc59
AsyrafFile <40899099+AsyrafFile@users.noreply.github.com>
2020-03-21 11:53:02 +0800

    Added galactic

---
+++ galactic/patch.js
@@ -0,0 +1,9 @@
+{
+	"dependencies": [
+	],
+	"id": "galactic",
+	"servers": [
+		"https://mirrors.thpatch.net/AsyrafFile/galactic/"
+	],
+	"title": "Replace non-diacritic Latin alphabet to Standard Galactic Alphabet from Commander Keen."
+}
--- repo.js
+++ repo.js
@@ -2,7 +2,8 @@
 	"contact": "AsyrafFile#8823",
 	"id": "AsyrafFile",
 	"patches": {
-	"ms-eastern": "Replace lang_ms western_name_order to eastern_name_order_hepburn."
+		"galactic": "Replace non-diacritic Latin alphabet to Standard Galactic Alphabet from Commander Keen.",
+		"ms-eastern": "Replace lang_ms western_name_order to eastern_name_order_hepburn."
 	},
 	"servers": [
 		"https://mirrors.thpatch.net/AsyrafFile/"
