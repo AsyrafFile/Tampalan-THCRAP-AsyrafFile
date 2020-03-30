c591be982156cb1ae0700943f3e666faa1161b5d
Zulhilmi Asyraf <40899099+AsyrafFile@users.noreply.github.com>
2020-03-30 16:13:29 +0800

    Update patch.js

--- galactic/patch.js
+++ galactic/patch.js
@@ -5,5 +5,5 @@
 	"servers": [
 		"https://mirrors.thpatch.net/AsyrafFile/galactic/"
 	],
-	"title": "Replace non-diacritic Latin alphabet to Standard Galactic Alphabet from Commander Keen. For now it still on progress"
+	"title": "Replace non-diacritic Latin alphabet to Standard Galactic Alphabet from Commander Keen and Minecraft. For now it still on progress"
 }
